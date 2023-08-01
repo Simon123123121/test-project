110427022ab5c9d6c94d38002dd645b3232954eb
Artemii Stankevich <artemstankevic688@gmail.com>
2023-08-01 13:57:34 +0300

    local branch fixed

--- python.py
+++ python.py
@@ -1,3 +1,9 @@
 print('goodbye world!!!')
 print('пока world!')
 print('Мир?')
+
+a = int(input('Привет мир, или пока мир? 1/2: '))
+if a == 1:
+    print('Привет мир')
+elif a == 2:
+    print('Пока мир')
